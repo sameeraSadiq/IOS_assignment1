be7cc2918bca02339163758c015bcd62389284b3
sameera sadiq <161041535+sameeraSadiq@users.noreply.github.com>
2025-01-26 12:08:36 -0500

    add

---
+++ IOSapp1/IOSapp1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOSapp1
+//
+//  Created by sameera sadiq on 2025-01-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOSapp1/IOSapp1/IOSapp1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSapp1App.swift
+//  IOSapp1
+//
+//  Created by sameera sadiq on 2025-01-26.
+//
+
+import SwiftUI
+
+@main
+struct IOSapp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
